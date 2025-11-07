8a9d65568ec6f6892018c3d14cff6952cb7f3d4f
whmccoy <whmccoy@gmail.com>
2025-11-07 20:02:44 +0000

    initial solution, one fetch call works

--- public/script.js
+++ public/script.js
@@ -10,5 +10,33 @@ btn.addEventListener("click", () => {
   msg.textContent = `Button clicked at ${now}`;
 });
    
+// returns the response JSON if the status is in the 200s
+// otherwise rejects the promise with a message including the status
+function checkStatus(response) {  
+        if (response.status >= 200 && response.status < 300) {  
+            return response.json();
+        } else if (response.status == 404) {
+            // sends back a different error when we have a 404 than when we have
+            // a different error
+            return Promise.reject(new Error("Sorry, we couldn't find that page")); 
+        } else {  
+            return Promise.reject(new Error(response.status + ": " + response.statusText)); 
+        } 
+    }
+
+
+   const BASE_URL = "https://legendary-succotash-v6qggjrw6q27qg-3000.app.github.dev/"; // REPLACE WITH PUBLIC URL TO YOUR CODESPACE OF BOOKS-SERVICE
+
+          fetch(BASE_URL)
+            .then(checkStatus)
+            .then(function(json) {
+                console.log(json); 
+                // add a radio button and text label for category 
+            })
+            .catch(function(error) {
+                console.log(error);
+            });
+
+
 }
 
